04e3633d435d3047f5bb740156abf962df317d87
Dan Edens <Danedens31@gmail.com>
2024-12-31 17:17:59 -0500

    Update mqtt_mind_map.rs

--- src/mqtt_mind_map.rs
+++ src/mqtt_mind_map.rs
@@ -221,7 +221,7 @@ fn main() {
     env_logger::init();
 
     let host = env::var("AWSIP2").unwrap_or_else(|_| "localhost".to_string());
-    let port = env::var("AWSPORT").unwrap_or_else(|_| "1884".to_string()).parse().unwrap_or(1884);
+    let port = env::var("AWSPORT").unwrap_or_else(|_| "3003".to_string()).parse().unwrap_or(3003);
 
     let mind_map = MQTTMindMap::new(host, port, 1.0, "output".to_string());
     mind_map.start();
